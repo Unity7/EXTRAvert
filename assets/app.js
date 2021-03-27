7ca46ead5d203264f388d2807c2b5757e1849605
Bay <baybui@ucla.edu>
2021-03-27 12:31:22 -0700

    updated html

---
+++ assets/app.js
@@ -0,0 +1,12 @@
+//  -----------------------------  Variables Section ----------------------------- //
+$searchInput = $("#search-input");
+$searchButton = $("#search-btn");
+$searchItems = $("#search-history-items");
+$eventsBtn = $("#events-btn");
+$flightsBtn = $("#flights-btn");
+$hotelsBtn = $("#hotels-btn");
+$resultItems = $("#result-items");
+
+//  -----------------------------  Functions ----------------------------- //
+
+//  -----------------------------  Event Listeners ----------------------------- //
