9b32c87ce29600f446e7008928a2246e83139299
Bay <baybui@ucla.edu>
2021-03-27 12:51:47 -0700

    app.js pseudo code added

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,49 @@ $flightsBtn = $("#flights-btn");
 $hotelsBtn = $("#hotels-btn");
 $resultItems = $("#result-items");
 
+cities = [];
+var searchInput, searchTerm;
+
 //  -----------------------------  Functions ----------------------------- //
 
+//Function to Execute Once "submit" has been clicked on search box//
+function getSearchTerm() {
+  event.preventDefault();
+  searchInput = $searchInput.val();
+  console.log(searchInput);
+
+  // getFlights()
+  // getEvents()
+  // getHotels()
+}
+
+//Uses API to get Flights//
+function getFlights() {
+  //fetch Data
+  console.log("Getting Flights");
+
+  //display Data
+  // displayResults(data);
+}
+
+//Uses API to get Events//
+function getEvents() {
+  console.log("Getting Events");
+
+  //display Data
+  // displayResults(data);
+}
+
+//Uses API to get Hotels//
+function getHotels() {
+  console.log("Getting Hotels");
+
+  //display Data
+  // displayResults(data);
+}
+
+function displayResults(data) {
+  console.log("Displaying Results");
+}
 //  -----------------------------  Event Listeners ----------------------------- //
+$searchButton.on("click", getSearchTerm);
